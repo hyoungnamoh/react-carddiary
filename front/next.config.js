afaf9dcefad183dc70b776b9fb07b743fae93f8e
hyungnam <zzzsh789@naver.com>
2020-04-16 14:26:25 +0900

    next.config.js 수정

--- front/next.config.js
+++ front/next.config.js
@@ -1,51 +1,24 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANAYZE === 'true',
+    enabled: process.env.ANALYZE === 'true',
 });
 const webpack = require('webpack');
 const CompressionPlugin = require('compression-webpack-plugin');
+
 module.exports = withBundleAnalyzer({
-
-    //BUNDLE_ANALYZE 라는 환경변수를 both 로 설정해 두면 프론트와 서버 둘 다 분석해줌
-    /*
-      package js 에서 넣는 방법
-      scripts 부분에 build 명령어 앞에 BUNDLE_ANALYZE=both 넣어주면 됨 => 윈도우에서 안됨, 맥이나 리눅스만 가능
-      => cross-env 설치하고 BUNDLE_ANALYZE=both 앞에 cross-env 넣어줌
-
-     */
-    // analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
-    // analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
-    // bundleAnalyzerConfig: {
-    //     server: {
-    //         analyzerMode: 'static',
-    //         reportFilename: '../../bundles/server.html'
-    //     },
-    //     browser: {
-    //         analyzerMode: 'static',
-    //         reportFilename: '../bundles/client.html'
-    //     }
-    // },
+    distDir: '.next',
     webpack(config) {
-        //NODE_ENV 배포환경인지 개발환경인지 구분
-        const prod = process.env.NODE_ENV === 'production'
-
+        const prod = process.env.NODE_ENV === 'production';
         const plugins = [
             ...config.plugins,
-            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/,/^\.\/ko$/), //moment js tree shaking
+            new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
         ];
-        if(prod){
-            plugins.push(new CompressionPlugin());
+        if (prod) {
+            plugins.push(new CompressionPlugin()); // main.js.gz
         }
-
-        //next 에서 넣어주는 바벨로더 등
         return {
-            ...config, //기존 설정을 리턴하고 덮어 씌움
-
+            ...config,
             mode: prod ? 'production' : 'development',
-
-            //hidden-source-map: 소스코드 숨기면서 에러 시 소스맵 제공
-            //eval: 빠르게 웹팩 적용
             devtool: prod ? 'hidden-source-map' : 'eval',
-
             plugins,
         };
     },
