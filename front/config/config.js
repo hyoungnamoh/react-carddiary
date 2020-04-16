a5a028718d213a245d0677b9deac8ee17850f318
hyungnam <zzzsh789@naver.com>
2020-04-16 18:26:34 +0900

    process.env.NODE_ENV 오류 수정중

--- front/config/config.js
+++ front/config/config.js
@@ -1,5 +1,5 @@
 //공용으로 사용할 axios base url
-// const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.22.56.62' : 'http://localhost:3603';
-const backUrl = 'http://3.22.56.62';
+const backUrl = process.env.NODE_ENV === 'production' ? 'http://18.219.58.174' : 'http://localhost:3603';
+// const backUrl = 'http://3.22.56.62';
 
 export { backUrl };
