5aa13999b3fec7f2cd6dc46d6c850628b7e25f3a
hyungnam <zzzsh789@naver.com>
2020-04-16 18:15:39 +0900

    backUrl 임시수정

--- front/config/config.js
+++ front/config/config.js
@@ -1,4 +1,5 @@
 //공용으로 사용할 axios base url
-const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.22.56.62' : 'http://localhost:3603';
+// const backUrl = process.env.NODE_ENV === 'production' ? 'http://3.22.56.62' : 'http://localhost:3603';
+const backUrl = 'http://3.22.56.62';
 
 export { backUrl };
