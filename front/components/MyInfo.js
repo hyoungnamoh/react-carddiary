f980f7843105605f5a3040638a6a1b14dc4a0281
hyungnam <zzzsh789@naver.com>
2020-04-17 14:13:42 +0900

    start script 수정

--- front/components/MyInfo.js
+++ front/components/MyInfo.js
@@ -46,7 +46,9 @@ const MyInfo = () => {
     const {isEditing, profileImagePath, personalUser, loginUser, followingList} = useSelector(state => state.user);
     const user = personalUser ? personalUser : loginUser;
     const [isFollowedUser, setIsFollowedUser] = useState(false);
-    
+    console.log(personalUser);
+
+
     //내가 팔로잉 하고있는 대상인지 확인
     useEffect(() => {
         if(followingList.length !== 0 && personalUser){
